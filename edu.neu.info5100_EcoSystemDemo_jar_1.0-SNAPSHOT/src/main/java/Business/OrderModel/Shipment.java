1dce2448a6cf10784989a64132cde172dc4ef362
Hannah Chiou <chiou.h@northeastern.edu>
2026-04-11 14:27:58 -0400

    adding new roles and classes

---
+++ edu.neu.info5100_EcoSystemDemo_jar_1.0-SNAPSHOT/src/main/java/Business/OrderModel/Shipment.java
@@ -0,0 +1,72 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Business.OrderModel;
+
+import java.util.Date;
+
+/**
+ *
+ * @author hannahchiou
+ */
+public class Shipment {
+
+    private String origin;
+    private String destination;
+    private Date departureDate;
+    private Date estimatedArrival;
+    private WholesaleOrder order;
+    private int shipmentID;
+    private static int counter = 0;
+
+    public Shipment() {
+        this.order = new WholesaleOrder();
+        this.shipmentID = ++counter;
+    }
+
+    public String getOrigin() {
+        return origin;
+    }
+
+    public void setOrigin(String origin) {
+        this.origin = origin;
+    }
+
+    public String getDestination() {
+        return destination;
+    }
+
+    public void setDestination(String destination) {
+        this.destination = destination;
+    }
+
+    public Date getDepartureDate() {
+        return departureDate;
+    }
+
+    public void setDepartureDate(Date departureDate) {
+        this.departureDate = departureDate;
+    }
+
+    public Date getEstimatedArrival() {
+        return estimatedArrival;
+    }
+
+    public void setEstimatedArrival(Date estimatedArrival) {
+        this.estimatedArrival = estimatedArrival;
+    }
+
+    public WholesaleOrder getOrder() {
+        return order;
+    }
+
+    public int getShipmentID() {
+        return shipmentID;
+    }
+
+    @Override
+    public String toString() {
+        return "Shipment #" + shipmentID + " | " + origin + " -> " + destination;
+    }
+}
